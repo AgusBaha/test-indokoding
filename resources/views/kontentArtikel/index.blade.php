ef4061a78be1c33321d23adced8fce807b6583f6
idkodekreatif <id.kodekreatif@gmail.com>
2024-01-13 16:33:09 +0700

    feat(post): like

--- resources/views/kontentArtikel/index.blade.php
+++ resources/views/kontentArtikel/index.blade.php
@@ -2,62 +2,91 @@
 
 @section('content')
 <div class="container">
-    @foreach($posts as $post)
+    <div class="row">
+        @foreach($posts as $post)
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card">
+                @if ($post->image)
+                <img src="{{ asset('storage/' . $post->image) }}" alt="Post Image" class="card-img-top img-fluid">
+                @endif
 
-    <h1>{{ $post->title }}</h1>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <h1>{{ $post->title }}</h1>
+                    </h5>
+                    <p class="card-text">
+                        @if (strlen($post->content) > 250)
+                        {!! substr($post->content, 0, 250) !!}
+                        <span id="read-more-{{ $post->id }}" class="read-more-content">...
+                            <a href="javascript:void(0);" onclick="expandContent('{{ $post->id }}')">Read more</a>
+                        </span>
+                        <span id="full-content-{{ $post->id }}" class="full-content" style="display: none;">
+                            {!! $post->content !!}
+                            <a href="javascript:void(0);" onclick="collapseContent('{{ $post->id }}')">Read less</a>
+                        </span>
+                        @else
+                        {!! $post->content !!}
+                        @endif
+                    </p>
 
-    @if ($post->image)
-    <img src="{{ asset('storage/' . $post->image) }}" alt="Post Image" class="img-fluid">
-    @endif
+                    @auth
+                    <button class="btn btn-primary" onclick="likePost('{{ $post->id }}')"><i
+                            class="far fa-heart"></i></button>
+                    @endauth
+                </div>
 
-    <p>{!! $post->content !!}</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Likes: <span id="like-count-{{ $post->id }}"
+                            data-post-id="{{ $post->id }}">{{ $post->likes }}</span></li>
+                    <li class="list-group-item">Posted by: {{ $post->author->name }}</li>
+                    <li class="list-group-item">Publish: {{ $post->created_at->format('M d, Y') }}</li>
+                    <li class="list-group-item">
+                        <h5>Comments:</h5>
+                        <ul>
+                            @foreach ($post->comments as $comment)
+                            <li data-comment-id="{{ $comment->id }}">
+                                <strong>{{ $comment->author->name }}</strong>: {{ $comment->content }}
+                                @auth
+                                <button class="btn btn-sm btn-secondary"
+                                    onclick="replyToComment({{ $comment->id }})">Reply</button>
+                                @endauth
 
-
-    <h4>Posted by: {{ $post->author->name }} on {{ $post->created_at->format('M d, Y') }}</h4>
-
-    <h5>Likes: <span id="like-count-{{ $post->id }}" data-post-id="{{ $post->id }}">{{ $post->likes }}</span></h5>
-    @auth
-    <button class="btn btn-primary" onclick="likePost('{{ $post->id }}')">Like</button>
-    @endauth
-
-    <h5>Comments:</h5>
-    <ul>
-        @foreach ($post->comments as $comment)
-        <li data-comment-id="{{ $comment->id }}">
-            <strong>{{ $comment->author->name }}</strong>: {{ $comment->content }}
-            @auth
-            <button class="btn btn-sm btn-secondary" onclick="replyToComment({{ $comment->id }})">Reply</button>
-            @endauth
-
-            @if ($comment->replies->count() > 0)
-            <ul class="list-unstyled">
-                @foreach ($comment->replies->sortByDesc('created_at') as $reply)
-                <li>
-                    <strong>{{ $reply->author->name }}</strong>: {{ $reply->content }}
-                </li>
-                @endforeach
-            </ul>
-            @endif
-        </li>
-        @endforeach
-    </ul>
-
-    @auth
-    <hr>
-    <h4>Leave a Comment:</h4>
-    <form action="{{ route('comment.store') }}" method="post">
-        @csrf
-        <input type="hidden" name="post_id" value="{{ $post->id }}">
-        <div class="form-group">
-            <label for="content">Comment</label>
-            <textarea name="content" id="content" class="form-control" rows="3"></textarea>
+                                @if ($comment->replies->count() > 0)
+                                <ul class="list-unstyled">
+                                    @foreach ($comment->replies->sortByDesc('created_at') as $reply)
+                                    <li>
+                                        <strong>{{ $reply->author->name }}</strong>: {{ $reply->content }}
+                                    </li>
+                                    @endforeach
+                                </ul>
+                                @endif
+                            </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    <li class="list-group-item">@auth
+                        <hr>
+                        <h4>Leave a Comment:</h4>
+                        <form action="{{ route('comment.store') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="post_id" value="{{ $post->id }}">
+                            <div class="form-group">
+                                <label for="content">Comment</label>
+                                <textarea name="content" id="content" class="form-control" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-2">Submit</button>
+                        </form>
+                        @endauth
+                    </li>
+                </ul>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    @endauth
+        @endforeach
 
-    <hr>
-    @endforeach
+        <div class="d-flex justify-content-center">
+            {{ $posts->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
@@ -99,6 +128,16 @@
 
             commentElement.appendChild(replyForm);
         }
+    }
+
+    function expandContent(postId) {
+        document.getElementById(`read-more-${postId}`).style.display = 'none';
+        document.getElementById(`full-content-${postId}`).style.display = 'inline';
+    }
+
+    function collapseContent(postId) {
+        document.getElementById(`read-more-${postId}`).style.display = 'inline';
+        document.getElementById(`full-content-${postId}`).style.display = 'none';
     }
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.3/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Scripts -->
